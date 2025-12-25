17f37cc66456b6842435e1f927adfc21104d81f8
Anushri <sastikaranushri@gmail.com>
2025-12-24 19:20:15 -0500

    corrections

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/merge-k-sorted-lists/
-# Time Complexity- O(N*k) Space Complexity- O(1)
+# Time Complexity- O(N log k) Space Complexity- O(k)
 
 import heapq
 
@@ -8,10 +8,20 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Why we need NodeWrapper
+# Python doesn’t know how to compare two ListNodes.
+# It will try to do list1 < list2
+# But ListNode has no < operator → TypeError
+
+
+# NodeWrapper wraps the ListNode.
+# __lt__ (less than) tells Python how to compare two wrapped nodes.
+# Now, the heap can always decide which node is smaller based on .val.
 class NodeWrapper:
     def __init__(self, node):
         self.node = node
-
+    # inbuilt method to implement functionality of the less than operator "<" .
+    # it returns the boolean value according to the condition
     def __lt__(self, other):
         return self.node.val < other.node.val
 
@@ -29,9 +39,12 @@ class Solution:
         for lst in lists:
             if lst is not None:
                  # add the list into the minHeap which has the property of heap,that is all the nodes are ordered in the ascending order
+                # heapq.heappush(minHeap, NodeWrapper(1 from L1))
+                # heapq.heappush(minHeap, NodeWrapper(1 from L2))
+                # heapq.heappush(minHeap, NodeWrapper(2 from L3))
                 heapq.heappush(minHeap, NodeWrapper(lst))
 
-        # iterate over the heap
+        # here the heap is not getting sorted but the linked list is getting sorted
         while minHeap:
             # pop function return the smallest elememnt from the heap
             min_node = heapq.heappop(minHeap)
